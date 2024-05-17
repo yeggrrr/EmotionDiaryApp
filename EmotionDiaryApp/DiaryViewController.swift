8b3ec8d74502b9a2b71e1de8c84a76e5cf0975b1
yeggrrr <yjkim981103@gmail.com>
2024-05-17 17:52:13 +0900

    [Feat] configureUI 추가

--- EmotionDiaryApp/DiaryViewController.swift
+++ EmotionDiaryApp/DiaryViewController.swift
@@ -52,6 +52,7 @@ class DiaryViewController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        addCount()
      
     }
 
@@ -59,6 +60,138 @@ class DiaryViewController: UIViewController {
         
         title = "감정 다이어리"
         
+        detailBarButtonItem.image = UIImage(systemName: "list.bullet")
+        
+        // happy
+        happyButton.setImage(UIImage(named: "행복"), for: .normal)
+        happyButton.backgroundColor = .clear
+        
+        happyLabel.text = "행복해"
+        happyLabel.textColor = .black
+        happyLabel.textAlignment = .center
+        happyLabel.font = .systemFont(ofSize: 17)
+        
+        happyCountLabel.text = "0"
+        happyCountLabel.textColor = .systemPink
+        happyCountLabel.textAlignment = .center
+        happyCountLabel.font = .systemFont(ofSize:  17)
+        
+        // love
+        loveButton.setImage(UIImage(named: "사랑"), for: .normal)
+        loveButton.backgroundColor = .clear
+        
+        loveLabel.text = "사랑해"
+        loveLabel.textColor = .black
+        loveLabel.textAlignment = .center
+        loveLabel.font = .systemFont(ofSize: 17)
+        
+        loveCountLabel.text = "0"
+        loveCountLabel.textColor = .systemPink
+        loveCountLabel.textAlignment = .center
+        loveCountLabel.font = .systemFont(ofSize:  17)
+        
+        
+        // like
+        likeButton.setImage(UIImage(named: "좋아"), for: .normal)
+        likeButton.backgroundColor = .clear
+        
+        likeLabel.text = "좋아해"
+        likeLabel.textColor = .black
+        likeLabel.textAlignment = .center
+        likeLabel.font = .systemFont(ofSize: 17)
+        
+        likeCountLabel.text = "0"
+        likeCountLabel.textColor = .systemPink
+        likeCountLabel.textAlignment = .center
+        likeCountLabel.font = .systemFont(ofSize:  17)
+        
+        // embarrass
+        embarrassButton.setImage(UIImage(named: "당황"), for: .normal)
+        embarrassButton.backgroundColor = .clear
+        
+        embarrassLabel.text = "당황해"
+        embarrassLabel.textColor = .black
+        embarrassLabel.textAlignment = .center
+        embarrassLabel.font = .systemFont(ofSize: 17)
+        
+        embarrassCountLabel.text = "0"
+        embarrassCountLabel.textColor = .systemPink
+        embarrassCountLabel.textAlignment = .center
+        embarrassCountLabel.font = .systemFont(ofSize:  17)
+        
+        // upset
+        upsetButton.setImage(UIImage(named: "속상"), for: .normal)
+        upsetButton.backgroundColor = .clear
+    
+        upsetLabel.text = "속상해"
+        upsetLabel.textColor = .black
+        upsetLabel.textAlignment = .center
+        upsetLabel.font = .systemFont(ofSize: 17)
+        
+        upsetCountLabel.text = "0"
+        upsetCountLabel.textColor = .systemPink
+        upsetCountLabel.textAlignment = .center
+        upsetCountLabel.font = .systemFont(ofSize:  17)
+        
+        // gloomy
+        gloomyButton.setImage(UIImage(named: "우울"), for: .normal)
+        gloomyButton.backgroundColor = .clear
+        
+        gloomyLabel.text = "당황해"
+        gloomyLabel.textColor = .black
+        gloomyLabel.textAlignment = .center
+        gloomyLabel.font = .systemFont(ofSize: 17)
+        
+        gloomyCountLabel.text = "0"
+        gloomyCountLabel.textColor = .systemPink
+        gloomyCountLabel.textAlignment = .center
+        gloomyCountLabel.font = .systemFont(ofSize:  17)
+        
+        // bored
+        boredButton.setImage(UIImage(named: "심심"), for: .normal)
+        boredButton.backgroundColor = .clear
+        
+        boredLabel.text = "심심해"
+        boredLabel.textColor = .black
+        boredLabel.textAlignment = .center
+        boredLabel.font = .systemFont(ofSize: 17)
+        
+        boredCountLabel.text = "0"
+        boredCountLabel.textColor = .systemPink
+        boredCountLabel.textAlignment = .center
+        boredCountLabel.font = .systemFont(ofSize:  17)
+        
+        // hungry
+        hungryButton.setImage(UIImage(named: "배고파"), for: .normal)
+        hungryButton.backgroundColor = .clear
+        
+        hungryLabel.text = "배고파"
+        hungryLabel.textColor = .black
+        hungryLabel.textAlignment = .center
+        hungryLabel.font = .systemFont(ofSize: 17)
+        
+        hungryCountLabel.text = "0"
+        hungryCountLabel.textColor = .systemPink
+        hungryCountLabel.textAlignment = .center
+        hungryCountLabel.font = .systemFont(ofSize:  17)
+        
+        // sad
+        sadButton.setImage(UIImage(named: "슬퍼"), for: .normal)
+        sadButton.backgroundColor = .clear
+        
+        sadLabel.text = "슬퍼해"
+        sadLabel.textColor = .black
+        sadLabel.textAlignment = .center
+        sadLabel.font = .systemFont(ofSize: 17)
+        
+        sadCountLabel.text = "0"
+        sadCountLabel.textColor = .systemPink
+        sadCountLabel.textAlignment = .center
+        sadCountLabel.font = .systemFont(ofSize:  17)
+        
+    }
+    
+    func addCount() {
         
     }
 
