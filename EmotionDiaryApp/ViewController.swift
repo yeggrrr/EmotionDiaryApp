a87affb1a7dbfbb06e7cb0e3af613ac6fc10970b
yeggrrr <yjkim981103@gmail.com>
2024-05-17 16:31:37 +0900

    Initial Commit

---
+++ EmotionDiaryApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EmotionDiaryApp
+//
+//  Created by YJ on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "감정 다이어리"
+     
+    }
+
+
+}
+
