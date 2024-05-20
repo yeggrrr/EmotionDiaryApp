314c58e12ca6844061735d3975d35e6636462338
yeggrrr <yjkim981103@gmail.com>
2024-05-21 00:32:14 +0900

    [Fix] 코드컨벤션 수정 & Array initializer 사용

--- EmotionDiaryApp/DiaryViewController.swift
+++ EmotionDiaryApp/DiaryViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class DiaryViewController: UIViewController {
-
     @IBOutlet var detailBarButtonItem: UIBarButtonItem!
     
     @IBOutlet var happyButton: UIButton!
@@ -49,82 +48,74 @@ class DiaryViewController: UIViewController {
     
     @IBOutlet var countLabelList: [UILabel]!
     
-    var count = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var count = Array<Int>.init(repeating: 0, count: 9)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationUI()
         configureUI()
-    
     }
 
     func configureUI() {
         // buttonUI
-        buttonUI(happyButton, image: "행복")
-        buttonUI(loveButton, image: "사랑")
-        buttonUI(likeButton, image: "좋아")
-        buttonUI(embarrassButton, image: "당황")
-        buttonUI(upsetButton, image: "속상")
-        buttonUI(gloomyButton, image: "우울")
-        buttonUI(boredButton, image: "심심")
-        buttonUI(hungryButton, image: "배고파")
-        buttonUI(sadButton, image: "슬퍼")
+        happyButton.setCustomImage("행복")
+        loveButton.setCustomImage("사랑")
+        likeButton.setCustomImage("좋아")
+        embarrassButton.setCustomImage("당황")
+        upsetButton.setCustomImage("속상")
+        gloomyButton.setCustomImage("우울")
+        boredButton.setCustomImage("심심")
+        hungryButton.setCustomImage("배고파")
+        sadButton.setCustomImage("슬퍼")
         
         // LabelUI
-        labelUI(happyLabel, "행복해")
-        labelUI(loveLabel, "사랑해")
-        labelUI(likeLabel, "좋아해")
-        labelUI(embarrassLabel, "당황해")
-        labelUI(upsetLabel, "속상해")
-        labelUI(gloomyLabel, "우울해")
-        labelUI(boredLabel, "심심해")
-        labelUI(hungryLabel, "배고파")
-        labelUI(sadLabel, "슬퍼해")
+        happyLabel.setCustomTitle("행복해")
+        loveLabel.setCustomTitle("사랑해")
+        likeLabel.setCustomTitle("좋아해")
+        embarrassLabel.setCustomTitle("당황해")
+        upsetLabel.setCustomTitle("속상해")
+        gloomyLabel.setCustomTitle("우울해")
+        boredLabel.setCustomTitle("심심해")
+        hungryLabel.setCustomTitle("배고파")
+        sadLabel.setCustomTitle("슬퍼해")
         
         // countLabelUI
-        countLabelUI(happyCountLabel)
-        countLabelUI(loveCountLabel)
-        countLabelUI(likeCountLabel)
-        countLabelUI(embarrassCountLabel)
-        countLabelUI(upsetCountLabel)
-        countLabelUI(gloomyCountLabel)
-        countLabelUI(boredCountLabel)
-        countLabelUI(hungryCountLabel)
-        countLabelUI(sadCountLabel)
-    }
-    
-    
-    func buttonUI(_ button: UIButton, image: String) {
-        button.setImage(UIImage(named: image), for: .normal)
-        button.backgroundColor = .clear
-    }
-    
-    func labelUI(_ label: UILabel, _ text: String) {
-        label.text = text
-        label.textColor = .black
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 17)
-    }
-    
-    func countLabelUI(_ countLabel: UILabel) {
-        countLabel.text = "0"
-        countLabel.textColor = .systemPink
-        countLabel.textAlignment = .center
-        countLabel.font = .systemFont(ofSize:  17)
+        [happyCountLabel, loveCountLabel, likeCountLabel, embarrassCountLabel, upsetCountLabel, gloomyCountLabel, boredCountLabel, hungryCountLabel, sadCountLabel].forEach {
+            $0.setCutomCount()
+        }
     }
     
     func navigationUI() {
-        
         title = "감정 다이어리"
         detailBarButtonItem.image = UIImage(systemName: "list.bullet")
     }
     
     @IBAction func emotionButtonClicked(_ sender: UIButton) {
-        
         count[sender.tag] += 1
         countLabelList[sender.tag].text = "\(count[sender.tag])"
-        
+    }
+}
+
+extension UIButton {
+    func setCustomImage(_ imageName: String) {
+        setImage(UIImage(named: imageName), for: .normal)
+        backgroundColor = .clear
+    }
+}
+
+extension UILabel {
+    func setCustomTitle(_ title: String) {
+        text = title
+        textColor = .black
+        textAlignment = .center
+        font = .systemFont(ofSize: 17)
+    }
+    
+    func setCutomCount() {
+        textColor = .systemPink
+        textAlignment = .center
+        font = .systemFont(ofSize:  17)
     }
 }
 
